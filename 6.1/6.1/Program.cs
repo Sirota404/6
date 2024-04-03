2a9f863a7f948dff8f8facf81c16b3b822824886
Sirota404 <153153025+Sirota404@users.noreply.github.com>
2024-04-03 10:00:46 +0800

    Add files via upload

---
+++ 6.1/6.1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+class Program
+{
+    static void Main()
+    {
+        var solarSystem = new[]
+        {
+new {Name = "Меркурий", OrderNumber = 1, EquatorLength = 5936, PreviousPlanet = new {Name = "Меркурий"}, EqualToVenus = false},
+new {Name = "Венера", OrderNumber = 2, EquatorLength = 12519, PreviousPlanet = new {Name = "Меркурий"}, EqualToVenus = true},
+new {Name = "Земля", OrderNumber = 3, EquatorLength = 12742, PreviousPlanet = new {Name = "Венера"}, EqualToVenus = false},
+new {Name = "Марс", OrderNumber = 4, EquatorLength = 6779, PreviousPlanet = new {Name = "Земля"}, EqualToVenus = false}
+};
+
+        foreach (var planet in solarSystem)
+        {
+            Console.WriteLine($"Планета: {planet.Name}, Порядковый номер от Солнца: {planet.OrderNumber}, Длина экватора: {planet.EquatorLength} км, Предыдущая планета: {planet.PreviousPlanet.Name}, Равна Венере: {(planet.EqualToVenus ? "да " : "нет ")}\n");
+        }
+    }
+}
